f1a10e2a0e7c2a0385531c58a0b7b11707ef29c7
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2025-05-18 17:14:08 +0300

    🤯 Beating my head over Golang... i like it

--- main.go
+++ main.go
@@ -8,8 +8,10 @@ import (
 	"github.com/joho/godotenv"
 )
 
-var PORT string
 var app *fiber.App
+
+var PORT string
+var API_KEY string
 
 func init() {
 
@@ -19,13 +21,12 @@ func init() {
 	}
 
 	PORT = os.Getenv("PORT")
-
-	log.Println(PORT);
+	API_KEY = os.Getenv("API_KEY")
 }
 
 func main() {
 	// Initialize the Fiber app with default settings
-	app := fiber.New()
+	app = fiber.New()
 
     // Define a route for the GET method on the root path '/'
     app.Get("/", func(c fiber.Ctx) error {
@@ -36,6 +37,44 @@ func main() {
 		})
     })
 
+	app.Post("/send", func(c fiber.Ctx) error {
+		c.Accepts("application/json")
+		c.Accepts("application/x-www-form-urlencoded")
+		
+		// Create a new email request struct
+		emailReq := new(EmailRequest)
+		
+		// Parse body into the struct
+		if err := c.Bind().Body(emailReq); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Failed to parse request body",
+			})
+		}
+		
+		// Now you can access the fields
+		apiKey := emailReq.APIKey
+		title := emailReq.Title
+		html := emailReq.HTML
+		text := emailReq.Text
+		author := emailReq.Author
+
+		return c.JSON(fiber.Map{
+			"apiKey": apiKey,
+			"title":  title,
+			"html":   html,
+			"text":   text,
+			"author": author,
+		});
+	})
+
     // Start the server on port 3000
     log.Fatal(app.Listen(":" + PORT))
+}
+
+type EmailRequest struct {
+    APIKey string `json:"apiKey" form:"apiKey"`
+    Title  string `json:"title" form:"title"`
+    HTML   string `json:"html" form:"html"`
+    Text   string `json:"text" form:"text"`
+    Author string `json:"author" form:"author"`
 }
